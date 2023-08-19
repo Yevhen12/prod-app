066854e23f4134fc86c87556c136dbb80a3344c2
Yevhen <epsonn537@gmail.com>
2023-08-19 23:11:30 +0300

    fix lints errors

--- src/shared/config/storybook/StyleDecorator.tsx
+++ src/shared/config/storybook/StyleDecorator.tsx
@@ -1,4 +1,4 @@
-import { Decorator, StoryObj, StoryFn } from '@storybook/react'
+import { StoryFn } from '@storybook/react'
 import 'app/styles/index.scss'
 
 export const StyleDecorator = (Story: StoryFn) => <Story />
