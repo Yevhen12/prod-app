c83f84abd1b5bb1f3960c0aeb103e38a2fc9a0eb
Yevhen <epsonn537@gmail.com>
2023-09-29 22:29:10 +0300

    add store decorator for Sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Theme } from 'app/providers/ThemeProvider'
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator'
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator'
 import Sidebar from './Sidebar'
 
@@ -21,8 +22,21 @@ type Story = StoryObj<typeof meta>
 export const Light: Story = {
   args: {}
 }
-
+Light.decorators = [StoreDecorator({
+  user: { authData: {} }
+})]
 export const Dark: Story = {
   args: {}
 }
-Dark.decorators = [ThemeDecorator(Theme.DARK)]
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+  user: { authData: {} }
+})]
+
+export const NoAuth: Story = {
+  args: {}
+}
+NoAuth.decorators = [StoreDecorator({
+  user: {
+    authData: undefined
+  }
+})]
