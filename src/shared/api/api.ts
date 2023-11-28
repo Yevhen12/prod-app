9c871d7833ccc73e429581fcb725c1fc93a6fbb2
Yevhen <epsonn537@gmail.com>
2023-11-28 19:04:32 +0200

    add interceptors

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,8 +3,13 @@ import axios from 'axios'
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage'
 
 export const api = axios.create({
-  baseURL: BASE_API_URL,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY)
+  baseURL: BASE_API_URL
+})
+
+api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? ''
   }
+
+  return config
 })
