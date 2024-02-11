144c281aad4e6a4bddd9d33f65c0b73c188576ea
Yevhen <epsonn537@gmail.com>
2024-02-11 17:20:55 +0200

    add host

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,6 +3,7 @@ import { Configuration as DevServerConfiguration } from 'webpack-dev-server'
 
 export const buildDevServer = (options: BuildOptions): DevServerConfiguration => {
   return {
+    host: '0.0.0.0',
     port: options.port,
     open: true,
     hot: true,
